310e396aba077601106ea2be43ae35c6a9726c76
nyju <nyju2104@gmail.com>
2021-09-10 20:28:34 +0900

    정리

--- src/main/java/com/withplant/config/SecutiryConfig.java
+++ src/main/java/com/withplant/config/SecutiryConfig.java
@@ -1,11 +1,9 @@
 package com.withplant.config;
 
 
-import com.withplant.config.auth.CustomOAuth2UserService;
-import com.withplant.member.MemberService;
+import com.withplant.member.auth.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
